d1a3ff0d5a1fb6997be3aac01d880aa2b06a2d05
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-03-19 18:22:30 +0100

    initial commit and setup

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './nav.css'
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testimonials.css'
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
