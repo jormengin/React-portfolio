c7dfe8058714f6dff42d0b6161e597fc69a399d1
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-03-21 15:59:40 +0100

    contact form, emailjs done, footer done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,92 @@
-import React from 'react'
-import './contact.css'
+/* eslint-disable react/jsx-no-target-blank */
+import React, { useRef,useState } from "react";
+import "./contact.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { ImWhatsapp } from "react-icons/im";
+import { TbBrandTelegram } from "react-icons/tb";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+  const [confirmationMessage, setConfirmationMessage] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_iq0x0f8",
+        "template_8fitsq3",
+        form.current,
+        "xUuCutitjN6Ey88-U"
+      )
+      .then(() => {
+        setConfirmationMessage(true);
+        setTimeout(() => {
+          setConfirmationMessage(false);
+        }, 5000);
+      });
+    e.target.reset();
+  };
   return (
-   <section id="contact">
-    contact
-   </section>
-  )
-}
+    <section id="contact">
+      <h5>Get in touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>jorge.mendezgines@gmail.com</h5>
+            <a href="mailto:jorge.mendezgines@gmail.com" target="_blank">
+              Mail me
+            </a>
+          </article>
+          <article className="contact__option">
+            <ImWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>+34615912335</h5>
+            <a href="https://wa.me/+34615912335 " target="_blank">
+              Message me
+            </a>
+          </article>
+          <article className="contact__option">
+            <TbBrandTelegram className="contact__option-icon" />
+            <h4>Telegram</h4>
+            <h5>@jormengin</h5>
+            <a href="https://t.me/jormengin" target="_blank">
+              Message Me
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your full name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your email" required />
+          <textarea
+            name="message"
+            id=""
+            // @ts-ignore
+            cols="30"
+            rows="10"
+            placeholder="Your message"
+            required
+          ></textarea>
+          {confirmationMessage && (
+        <div className="confirmation-message">
+          <p>Your message has been sent successfully! Thank you.</p>
+        </div>
+      )}
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+      
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import './footer.css'
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./footer.css";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+import { CgShutterstock } from "react-icons/cg";
 
-const About = () => {
+const Footer = () => {
   return (
-    <div>About</div>
-  )
-}
+    <footer>
+      <a href="" className="footer__logo">JORGE MÉNDEZ</a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a
+          href="https://www.linkedin.com/in/jorge-m%C3%A9ndez-51b928124/"
+          target="_blank"
+        >
+          <AiFillLinkedin />
+        </a>
+        <a href="https://github.com/jormengin" target="_blank">
+          <AiFillGithub />
+        </a>
+        <a href="https://wirestock.io/jorge.mendez" target="_blank">
+          <CgShutterstock />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-export default About
+export default Footer;
