ab98149751e31bd55bbe1eb907427f24299edc47
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-03-25 10:16:49 +0100

    pdf logo added

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -2,11 +2,12 @@
 
 import React from 'react'
 import cv from '../../assets/JorgeMendezCv.pdf'
+import {AiOutlineFilePdf} from 'react-icons/ai'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={cv} download className='btn'>Download CV</a>
+        <a href={cv} download className='btn'>Download CV <AiOutlineFilePdf className='pdflogo'/></a>
         <a href="#contact" className='btn btn-primary'>Let's talk</a>
     </div>
   )
